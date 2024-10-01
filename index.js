79ae9161a52665f1a9a16ea1eee50a87d98eaafc
Pathipaka Sai Krishna <118557421+saikrishna-1611@users.noreply.github.com>
2024-10-01 23:53:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+import qr from "qr-image";
+import inquirer from "inquirer";
+import { url } from "inspector";
+import fs from "fs";
+inquirer
+  .prompt([
+   { message : "type in your URL",
+    name : "URL",
+  },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+  
+ 
